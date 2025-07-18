f1a97b8d1601c3050bc6d9a5c1f02727bf0648f2
Nico Wu <>
2025-07-18 14:07:14 +0800

    fix header

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true, limit: "200kb" }));
 
 // 加入 CORS header
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 })
 
