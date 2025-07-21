9643f2b9f1731453096a4909ffee3b0969372a3f
Nico Wu <>
2025-07-21 16:02:54 +0800

    fix

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "200kb" }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+})
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
