67a2b1362965cc5ec53bce76ee030c8f7b31bf57
Nico Wu <>
2025-07-25 17:14:32 +0800

    test

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "200kb" }));
 
-// app.use(cors({
-//   origin: "*",
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // app.options('*', cors());
 
