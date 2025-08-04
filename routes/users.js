8ebd7fc3d5d36f8d2ae1d8ebc3bb3904b4f7906d
Nico Wu <>
2025-08-04 14:03:43 +0800

    test

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const pool = require("../db.js")
 pool.query(`
   CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    email TEXT
+    username TEXT,
+    info JSONB
   );
 `);
 
